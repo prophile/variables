cef04156327ecd1e349338790769ebcd7e67a270
Alistair Lynn <arplynn@gmail.com>
2014-08-18 00:58:36 +0100

    Add instance for STM

--- Control/Monad/Variables.hs
+++ Control/Monad/Variables.hs
@@ -3,11 +3,16 @@ module Control.Monad.Variables(Variable(Variable, load, store),
                                MonadVar,
                                stateVar) where
 
+-- From base
 import Control.Monad.ST
 import Data.IORef
 import Data.STRef
 
+-- From mtl
 import Control.Monad.State.Class
+
+-- From stm
+import Control.Concurrent.STM
 
 -- | The type of variables. In contrast with other "monads-with-variables"
 -- packages, this is not done using type families.
@@ -19,6 +24,7 @@ class Monad m => MonadVar m where
   -- | Create a variable from an initial value.
   newVar :: a -> m (Variable m a)
 
+-- Base instances
 instance MonadVar IO where
   newVar x = do var <- newIORef x
                 return Variable { load  = readIORef var,
@@ -28,6 +34,11 @@ instance MonadVar (ST s) where
   newVar x = do var <- newSTRef x
                 return Variable { load  = readSTRef var,
                                   store = writeSTRef var }
+
+instance MonadVar STM where
+  newVar x = do var <- newTVar x
+                return Variable { load  = readTVar var,
+                                  store = writeTVar var }
 
 -- | Access a variable representing the state of a state monad.
 stateVar :: MonadState s m => Variable m s
