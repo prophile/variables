f0b660c33951c5c1ae16113ff588d61ea76e0650
Alistair Lynn <arplynn@gmail.com>
2014-08-18 00:51:29 +0100

    Add very rough documentation

--- Control/Monad/Variables.hs
+++ Control/Monad/Variables.hs
@@ -1,11 +1,16 @@
-module Control.Monad.Variables(Variable(Variable), load, store, MonadVar) where
+-- | Monads with variables.
+module Control.Monad.Variables(Variable(Variable, load, store), MonadVar) where
 
 import Data.IORef
 
+-- | The type of variables. In contrast with other "monads-with-variables"
+-- packages, this is not done using type families.
 data Variable m a = Variable { load  :: m a,
                                store :: a -> m () }
 
+-- | Monads which admit general variables.
 class Monad m => MonadVar m where
+  -- | Create a variable from an initial value.
   newVar :: a -> m (Variable m a)
 
 instance MonadVar IO where
