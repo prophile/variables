50dcb6033d6b51ce69de9992ff705e7c252bae57
Alistair Lynn <arplynn@gmail.com>
2014-08-18 00:55:40 +0100

    Add instance for ST

--- Control/Monad/Variables.hs
+++ Control/Monad/Variables.hs
@@ -3,7 +3,10 @@ module Control.Monad.Variables(Variable(Variable, load, store),
                                MonadVar,
                                stateVar) where
 
+import Control.Monad.ST
 import Data.IORef
+import Data.STRef
+
 import Control.Monad.State.Class
 
 -- | The type of variables. In contrast with other "monads-with-variables"
@@ -18,8 +21,13 @@ class Monad m => MonadVar m where
 
 instance MonadVar IO where
   newVar x = do var <- newIORef x
-                return Variable { load = readIORef var,
+                return Variable { load  = readIORef var,
                                   store = writeIORef var }
+
+instance MonadVar (ST s) where
+  newVar x = do var <- newSTRef x
+                return Variable { load  = readSTRef var,
+                                  store = writeSTRef var }
 
 -- | Access a variable representing the state of a state monad.
 stateVar :: MonadState s m => Variable m s
