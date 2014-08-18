6e4a6b7b873df97eed1c2e619c32272222208796
Alistair Lynn <arplynn@gmail.com>
2014-08-18 01:46:30 +0100

    Add STM tests

--- Tests.hs
+++ Tests.hs
@@ -6,6 +6,8 @@ import Test.QuickCheck.Function
 
 import Control.Monad.Variables
 
+import Control.Concurrent.STM
+
 main :: IO ()
 main = hspec $ do
   describe "the IO instance" $ do
@@ -17,4 +19,18 @@ main = hspec $ do
       var <- newVar 2
       writeVar var 3
       readVar var `shouldReturn` 3
+
+  describe "the STM instance" $ do
+    it "supports variable creation" $ do
+      result <- atomically $ do
+        var <- newVar 2
+        readVar var
+      result `shouldBe` 2
+
+    it "supports variable modification" $ do
+      result <- atomically $ do
+        var <- newVar 2
+        writeVar var 3
+        readVar var
+      result `shouldBe` 3
 
